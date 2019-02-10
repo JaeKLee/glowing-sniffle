d74ff85863a154beb54095d8d3cc70aa91059a39
JaeKLee <jae.lee2@marist.edu>
2019-02-10 17:26:29 -0500

    Still broken

--- lex.py
+++ lex.py
@@ -1,28 +1,51 @@
 # Author: Jae Kyoung Lee (LJ)
 import re
+# Importing list of our grammar regex from regex.py
 import regex
 
-# Opening as read mode because all I need is to read for now
+# Opening as read mode to read the test files
 open_file = open("test_alan.txt", "r")
 
-# testArray = open_file.readlines()
-# print(len(testArray))
-# print(testArray)
+listFile = open_file.read().split(" ")
+# print(listFile)
 
 lineNumber = 1
 programNumber = 1
+# for line in listFile: 
+#   print(line)
+#   if re.compile(regex.leftParen).match(line):
+#     print("Found token left paren:    " , line , " in line " , lineNumber)
+print(listFile)
 
-# for each line in the test file
-for line in open_file:
-  # print(lineNumber)    
-  print("Program ", programNumber)
-  for character in line: 
-    if re.search(regex.leftParen, character):
-      print("Found left paren:  " , character , " in line " , lineNumber)
-    elif re.search(regex.rightParen, character):
-      print("Found right paren: " , character , " in line " , lineNumber)
-    elif re.search(regex.eop, character):
-      print("End of Program detected: ", character)
-  programNumber+=1
+
+for line in listFile:
+  # print("Program ", programNumber)
+  # for character in line: 
+    # print(character)
+  if re.search(regex.leftParen, line):
+    print("In line " , lineNumber, "found token left paren:    " , line)
+  elif re.search(regex.rightParen, line):
+    print("Found token right paren:   " , line , " in line " , lineNumber)
+  elif re.search(regex.character, line):
+    print("character: ", line , " in line " , lineNumber)
+  elif re.compile(regex.typeInt).findall(line):
+    print("Found token type Integer:  " , line , " in line " , lineNumber)
+  elif re.search(regex.typeString, line):
+    print("Found token type String:   " , line , " in line " , lineNumber)
+  elif re.search(regex.typeboolean, line):
+    print("Found token type Boolean:  " , line , " in line " , lineNumber)
+  elif re.search(regex.equal, line):
+    print("Found token equal:         " , line , " in line " , lineNumber)
+  elif re.search(regex.notEqual, line):
+    print("Found token not Equal:     " , line , " in line " , lineNumber)
+  elif re.search(regex.boolFalse, line):
+    print("Found token boolean false: " , line , " in line " , lineNumber)
+  elif re.search(regex.boolTrue, line):
+    print("Found token boolean True:  " , line , " in line " , lineNumber)
+  elif re.search(regex.assign, line):
+    print("Found token assignment:    " , line , " in line " , lineNumber)
+  # elif re.compile(regex.eop).match(line):
+  #   print("End of Program detected:  ", line, "\n")  
+  #   programNumber+=1
   lineNumber+=1
 
