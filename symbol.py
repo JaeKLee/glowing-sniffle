86d9c6a068418e891c42a37a3bd4981b8f452794
JaeKLee <jae.lee2@marist.edu>
2019-04-17 03:22:56 -0400

    save me

--- symbol.py
+++ symbol.py
@@ -5,3 +5,15 @@ class Symbol:
     self.type = type
     self.category = category
 
+class BuiltinTypeSymbol(Symbol):
+  def __init__(self, name):
+    super(BuiltinTypeSymbol, self).__init__(name)  
+
+  def __str__(self):
+    return self.name
+
+  def __repr__(self):
+    return "<{class_name}(name='{name}')>".format(
+      class_name=self.__class__.__name__,
+      name=self.name,
+    )
